b025f80f57b3b5666cf8814b9a03317818612730
DAH9 <danila.titov1210@yandex.ru>
2021-06-01 00:25:52 +0300

    fixed
    O(1), delete[], destructor, more information

--- homework5/CircularBuffer.hpp
+++ homework5/CircularBuffer.hpp
@@ -1,15 +1,22 @@
 #pragma once
 
+using namespace std;
+
 template<class T>
 class CircularBuffer {
 private:
-    int size = 0;
-    int capacity = 0;
-    T *elements = nullptr;
+    size_t bufSize = 0;
+    size_t capacity = 0;
+    size_t bufStart = 0;
+    size_t finish = 0;
+    T *bufElements = nullptr;
 public:
     class Iterator {
     private:
         T *elements = nullptr;
+        size_t size;
+        size_t index;
+        size_t start;
     public:
         using iterator_category = std::random_access_iterator_tag;
         using value_type = T;
@@ -17,156 +24,192 @@ public:
         using pointer = T*;
         using reference = T&;
 
-        explicit Iterator(T *start) {
-            elements = start;
+        explicit Iterator(T *tempElements, size_t tempSize, size_t tempIndex, size_t tempStart) {
+            elements = tempElements;
+            size = tempSize;
+            index = tempIndex;
+            start = tempStart;
         }
 
         Iterator(const Iterator &other) {
             elements = other.elements;
+            size = other.size;
+            index = other.index;
+            start = other.start;
         }
 
-        ~Iterator() = default;
+        ~Iterator() {
+            delete[] elements;
+        }
 
-        Iterator operator+(value_type tempNum) {
-            elements += tempNum;
+        Iterator &operator+=(difference_type tempNum) {
+            index += tempNum;
             return *this;
         }
 
-        Iterator operator++() {
-            elements++;
+        Iterator operator+(difference_type tempNum) const {
+            Iterator temp = *this;
+            temp += tempNum;
+            return temp;
+        }
+
+        Iterator &operator++() {
+            index++;
             return *this;
         }
 
-        Iterator operator-(value_type tempNum) {
-            elements -= tempNum;
+        Iterator &operator-=(difference_type tempNum) {
+            index -= tempNum;
             return *this;
         }
 
-        value_type operator-(const Iterator &other) const {
-            return elements - other.elements;
+        Iterator operator-(difference_type tempNum) const {
+            Iterator temp = *this;
+            temp -= tempNum;
+            return temp;
         }
 
-        Iterator operator--() {
-            elements--;
+        difference_type operator-(const Iterator &tempNum) const {
+            return index - tempNum.index;
+        }
+
+        Iterator &operator--() {
+            index--;
             return *this;
         }
 
-        reference operator*() const {
-            return *(elements);
+        reference operator*() {
+            return elements[(start + index) % size];
         }
 
         pointer operator->() {
-            return elements;
+            return elements + (start + index) % size;
         }
 
         bool operator==(const Iterator &other) const {
-            return elements == other.elements;
+            return index == other.index;
         }
 
         bool operator!=(const Iterator &other) const {
-            return elements != other.elements;
+            return index != other.index;
         }
 
         bool operator<(const Iterator &other) const {
-            return elements < other.elements;
+            return index < other.index;
         }
 
         bool operator>(const Iterator &other) const {
-            return elements > other.elements;
+            return index > other.index;
         }
 
         bool operator<=(const Iterator &other) const {
-            return elements <= other.elements;
+            return index <= other.index;
         }
 
         bool operator>=(const Iterator &other) const {
-            return elements >= other.elements;
+            return index >= other.index;
         }
     };
 
-    explicit CircularBuffer(int tempCapacity) {
+    explicit CircularBuffer(size_t tempCapacity) {
         capacity = tempCapacity + 1;
-        elements = new T[capacity];
+        bufElements = new T[tempCapacity + 1];
+        bufSize = 0;
+        bufStart = 0;
+        finish = 0;
     }
 
     CircularBuffer(const CircularBuffer &other) {
-        size = other.size;
         capacity = other.capacity;
-        elements = other.elements;
-    }
-    //todo delete
-    ~CircularBuffer() = default;
-    //todo more information in exceptions
-    T &operator[](int index) const {
-        if (size == 0)
-            throw std::out_of_range("Error: empty container");
-        if (index >= size)
-            throw std::out_of_range("Error: index is out of range");
-        return elements[index % capacity];
-    }
-    //todo O(1)
-    void addFirst(T first) {
-        if (size < capacity - 1) {
-            for (int i = size; i >= 1; i--)
-                elements[i] = elements[i - 1];
-            elements[0] = first;
-            size++;
-        } else {
-            for (int i = capacity - 2; i >= 1; i--)
-                elements[i] = elements[i - 1];
-            elements[0] = first;
+        bufElements = new T[bufSize];
+        size = other.size;
+        bufStart = other.start;
+        finish = 0;
+
+        for (size_t i = 0; i < bufSize; i++) {
+            *bufElements[i] = other.elements[i];
         }
+    }
+
+    //fixed delete
+    ~CircularBuffer() {
+        delete[] bufElements;
+    }
+
+    //fixed more information in exceptions
+    T &operator[](size_t index) const {
+        if (bufSize == 0)
+            throw std::out_of_range("Error: empty container");
+        if (index >= bufSize)
+            throw std::out_of_range(
+                    "Error: index is out of range ; index must be in range 0 - " + std::to_string(bufSize - 1) +
+                    " ; your index: " + std::to_string(index));
+        return bufElements[((index % bufSize) + bufStart) % capacity];
+    }
+
+    //fixed O(1)
+    void addFirst(T first) {
+        bufStart = (capacity + bufStart - 1) % capacity;
+        bufElements[bufStart] = first;
+
+        if (bufSize == capacity - 1)
+            finish = (capacity + finish - 1) % capacity;
+        else
+            bufSize++;
     }
 
     void addLast(T last) {
-        if (size < capacity - 1) {
-            elements[size] = last;
-            size++;
-        } else {
-            for (int i = 1; i < capacity - 1; i++)
-                elements[i - 1] = elements[i];
-            elements[capacity - 2] = last;
-        }
+        bufElements[finish] = last;
+        finish = (finish + 1) % capacity;
+
+        if (bufSize == capacity - 1)
+            bufStart = (bufStart + 1) % capacity;
+        else
+            bufSize++;
     }
 
     void delFirst() {
-        for (int i = 0; i < size - 1; i++)
-            elements[i] = elements[i + 1];
-        size--;
-    }
-
-    void delLast() {
-        if (size > 0)
-            size--;
-    }
-
-    void changeCapacity(int tempCapacity) {
-        if (tempCapacity < size) {
-            throw std::logic_error("Error: not enough capacity");
-        } else {
-            T *tempElements = new T[tempCapacity + 1];
-            for (int i = 0; i < size; i++)
-                tempElements[i] = elements[i];
-            //todo delete[]
-            delete elements;
-            elements = tempElements;
-            capacity = tempCapacity + 1;
+        if (bufSize > 0) {
+            bufSize--;
+            bufStart = (bufStart + 1) % capacity;
         }
     }
 
+    void delLast() {
+        if (bufSize > 0) {
+            bufSize--;
+            finish = (capacity + finish - 1) % capacity;
+        }
+    }
+
+    void changeCapacity(size_t tempCapacity) {
+        if (tempCapacity < bufSize)
+            throw std::logic_error("Error: not enough capacity");
+
+        T *tempElements = new T[tempCapacity + 1];
+        for (size_t i = 0; i < bufSize; i++)
+            tempElements[i] = bufElements[i];
+        //fixed delete[]
+        delete[] bufElements;
+        bufElements = tempElements;
+        capacity = tempCapacity + 1;
+        bufStart = 0;
+        finish = bufSize % capacity;
+    }
+
     Iterator begin() const {
-        return Iterator(elements);
+        return Iterator(bufElements, capacity, 0, bufStart);
     }
 
     Iterator end() const {
-        return Iterator(elements + size);
+        return Iterator(bufElements, capacity, bufSize, bufStart);
     }
 
     T first() const {
-        return elements[0];
+        return bufElements[bufStart];
     }
 
     T last() const {
-        return elements[size - 1];
+        return bufElements[(bufSize + bufStart - 1) % capacity];
     }
 };
